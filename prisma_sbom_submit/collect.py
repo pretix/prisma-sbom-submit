410396a6266f77f353cb273dc512292d8fb65600
Raphael Michel <michel@pretix.eu>
2026-08-25 15:02:17 +0200

    Fix running on GitHub (#1)
    * Add CI
    
    * FIx branch name
    
    * Fix import
    
    * Job name
    
    * USe shell
    
    * test stupid stuff
    
    * Use bash stuff
    
    * Raise
    
    * Add install
    
    * Tool dir

--- prisma_sbom_submit/collect.py
+++ prisma_sbom_submit/collect.py
@@ -4,13 +4,14 @@ import json
 import subprocess
 import tempfile
 import sys
+import shlex
 
 
 def discover_python_projects(path):
     if os.path.exists(os.path.join(path, "pyproject.toml")):
         yield path
     for p in glob.glob("**/pyproject.toml", root_dir=path):
-        yield os.dirname(os.path.join(path, p))
+        yield os.path.dirname(os.path.join(path, p))
 
 
 def generate_python_sbom(path):
@@ -33,26 +34,20 @@ def discover_npm_projects(path):
     if os.path.exists(os.path.join(path, "package.json")):
         yield path
     for p in glob.glob("**/package.json", root_dir=path):
-        yield os.dirname(os.path.join(path, p))
+        yield os.path.dirname(os.path.join(path, p))
 
 
 def generate_npm_sbom(path):
     cwd = os.getcwd()
     npm_tool_dir = os.path.join(cwd, ".sbom-npm-tools")
     npm = os.environ.get("NPM", "npm")
-    subprocess.check_call(
-        [npm, "install", "--global", "@cyclonedx/cyclonedx-npm"],
-        env={
-            "NPM_CONFIG_PREFIX": npm_tool_dir
-        },
-    )
+    # No idea why the explicit bash call is necessary, only way I could make it work on GitHub actions
+    subprocess.check_call(["bash", "-c", f"{shlex.quote(npm)} install --global --prefix {shlex.quote(npm_tool_dir)} @cyclonedx/cyclonedx-npm"])
     try:
         os.chdir(path)
-        subprocess.check_call([npm, "ci"])
+        subprocess.check_call(["bash", "-c", f"{shlex.quote(npm)} ci"])
         print(f"Generate SBOM")
-        sbom = subprocess.check_output(
-            [os.path.join(npm_tool_dir, "bin", "cyclonedx-npm")],
-        )
+        sbom = subprocess.check_output(["bash", "-c", f'{shlex.quote(os.path.join(npm_tool_dir, "bin", "cyclonedx-npm"))}'])
     finally:
         os.chdir(cwd)
     return json.loads(sbom.strip())
